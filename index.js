100f6c1f56c571e4090388fa98f0d51108c4d348
Stranger in the Q <tripmann@gmail.com>
2016-07-09 02:24:34 +0300

    position fixed

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var header = document.querySelector('.header');
 
 document.addEventListener('scroll', onScroll);
+onScroll();
 
 function onScroll() {
-    if (document.body.scrollTop < 500) {
-        header.style.height = Math.min(100, document.body.scrollTop) + "px";
-    }
+    header.style.height = Math.min(100, 500 - document.body.scrollTop) + "px";
 }
