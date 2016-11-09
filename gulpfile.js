2a89e5a1abb1ab9875fce46b4f480d6886deb1d2
Balashev Konstantin <tripmannn@gmail.com>
2016-11-09 12:06:48 +0300

    nuga best build with gulp

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+
+var Task = require('gulp-task-builder');
+
+new Task('nuga-best-1')
+    .src('projects/nuga-best/v1/src/index.html')
+    .fileinclude()
+    .dest('projects/nuga-best/v1');
+
+new Task('nuga-best-2')
+    .src('projects/nuga-best/v2/src/index.html')
+    .fileinclude()
+    .dest('projects/nuga-best/v2');
+
+new Task('nuga-best-3')
+    .src('projects/nuga-best/v3/src/index.html')
+    .fileinclude()
+    .dest('projects/nuga-best/v3');
+
+new Task('default')
+    .depends([
+        'nuga-best-1', 'nuga-best-2', 'nuga-best-3'
+    ])
+    .src('home/src/*.html')
+    .fileinclude()
+    .dest('home');
+
