ceae95982a636645174c3c3fdef482eaead10793
Stranger_in_the_Q <tripmannn@gmail.com>
2021-05-10 21:09:52 +0300

    brush

--- hen/brush/code.js
+++ hen/brush/code.js
@@ -1,39 +1,34 @@
-const debug = 0;
+const debug = 1;
 
 clear();
 
-const pointsCount = 2*(2 + randomInt(3));
-const points = many(pointsCount, () => [124 + random(size-248), random(size-248)]);
-
-
+const pointsCount = 2*(2 + randomInt(5));
+const points = many(pointsCount, () => [124 + random(size-248), 124 + random(size-248)]);
 
 for (let i = 0; i < points.length; i += 2) {
 
     const colorsCount = 2 + randomInt(30);
-    const alpha = 0.4*Math.random()*Math.random() + 0.1;
+    const alpha = 0.4*Math.random()*Math.random() + 0.1  + random(0.4);;
     const colors = many(colorsCount, () => hsla(random(360), 55, 55, alpha));
 
     const total = len(...points[i], ...points[i + 1]);
     const k = random(total/4)-total/8
-
+    const spr = 10 + random(20)
     colors.forEach((color, index) => {
         ctx.fillStyle = color;
-        randomArc(...points[i], ...points[i + 1], index/30, k,colors)
+        arc(...points[i], ...points[i + 1], index/30, k, colors, spr)
     })
 
     if (debug) {
-        const a = dir(...points[i], ...points[i + 1]) + Math.PI/2;
-        const cs = cos(a)
-        const sn = sin(a)
-        const p = 0.5*colors.length*5;
-        line(...points[i], ...points[i + 1]);
-
-        [points[i], points[i+1]].forEach(pt =>
-            line(-p*cs+pt[0], -p*sn+pt[1], p*cs+pt[0], p*sn+pt[1]))
+        debugArc(...points[i], ...points[i + 1], colors.length*5)
     }
 }
 
-function randomArc(x1, y1, x2, y2, t, k, colors) {
+function ellipse(x,y,r){
+
+}
+
+function arc(x1, y1, x2, y2, t, k, colors, spr) {
 
     const a = dir(x1, y1, x2, y2) + Math.PI/2;
     const cs = cos(a)
@@ -41,8 +36,8 @@ function randomArc(x1, y1, x2, y2, t, k, colors) {
 
     const p = (t - 0.5)*colors.length*5;
 
-    const dx = random(30, 15) + p*cs
-    const dy = random(30, 15) + p*sn
+    const dx = random(spr, spr/2) + p*cs
+    const dy = random(spr, spr/2) + p*sn
 
     x1 += dx;
     y1 += dy;
@@ -55,12 +50,19 @@ function randomArc(x1, y1, x2, y2, t, k, colors) {
 
     while (current < total) {
         const t = current / total;
-        const r = random(5) + 2;
+        const r = random(1) + 1;
         const f = sin(t*Math.PI)*k;
         const x = lerp(x1, x2, t) + random(4) + cs * f;
         const y = lerp(y1, y2, t) + random(4) + sn * f;
         circle(x, y, r);
-        current += r;
+        current += r*2;
     }
 }
 
+function debugArc(x1, y1, x2, y2, s){
+    const a = dir(x1, y1, x2, y2) + Math.PI/2;
+    line(x1, y1, x2, y2);
+    const x = 0.5*s*cos(a);
+    const y = 0.5*s*sin(a);
+    [[x1, y1], [x2, y2]].forEach(pt => line(-x+pt[0], -y+pt[1], x+pt[0], y+pt[1]))
+}
