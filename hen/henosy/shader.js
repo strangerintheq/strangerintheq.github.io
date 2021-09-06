901d44cbefa2edef1e48e1eb49e534c6f67d5e16
Konstantin Balashev <tripmannn@gmail.com>
2021-09-06 10:39:31 +0300

    gen

---
+++ hen/henosy/shader.js
@@ -0,0 +1,62 @@
+
+const c = document.querySelector('canvas'),
+    gl = c.getContext('webgl'),
+    p = gl.createProgram();
+
+shader(window.vertexShader, gl.VERTEX_SHADER);
+
+shader(window.fragmentShader, gl.FRAGMENT_SHADER);
+
+gl.linkProgram(p)
+gl.useProgram(p)
+
+const vertex = gl.getAttribLocation(p, "vertex"),
+    resolution = gl.getUniformLocation(p, 'resolution')
+
+gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer())
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([-1,3,-1,-1,3,-1]), gl.STATIC_DRAW)
+gl.vertexAttribPointer(vertex, 2, gl.FLOAT, false, 0, 0)
+gl.enableVertexAttribArray(vertex)
+
+let w, h;
+function resize(){
+    if (innerWidth === w && innerHeight === h)
+        return
+    w = canvas.width = innerWidth;
+    h = canvas.height = innerHeight;
+    gl.viewport(0, 0, w, h);
+    gl.uniform2f(resolution, w, h);
+}
+
+resize();
+
+function shader(src,a) {
+    let s = gl.createShader(a)
+    gl.shaderSource(s,src)
+    gl.compileShader(s)
+    if (!gl.getShaderParameter(s, gl.COMPILE_STATUS))
+        throw new Error(gl.getShaderInfoLog(s));
+    gl.attachShader(p,s)
+}
+
+const uniformsLocations = {};
+
+function ensureUniform(name) {
+    if (!uniformsLocations[name])
+        uniformsLocations[name] = gl.getUniformLocation(p, name)
+}
+
+function uf3(name, a, b, c) {
+    ensureUniform(name);
+    gl.uniform3f(uniformsLocations[name], a, b, c)
+}
+
+function uf2(name, a, b) {
+    ensureUniform(name);
+    gl.uniform2f(uniformsLocations[name], a, b)
+}
+
+function uf1(name, a){
+    ensureUniform(name);
+    gl.uniform1f(uniformsLocations[name], a);
+}
