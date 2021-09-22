77a36e971b74d5b9e2336ded5056de5e291edd41
k.balashev <k.balashev@geoscan.aero>
2021-09-22 10:46:07 +0300

    cleanup

--- hen/shader/main.js
+++
@@ -1,7 +0,0 @@
-uf1('zoom', 0.25+Math.random()*0.25);
-uf2('center', Math.random()-0.5, Math.random()-0.5)
-uf2('mn', 0.31 + Math.random()*0.1, -0.71+ Math.random()*0.1)
-uf2('ab', Math.random(), Math.random())
-uf3('color', Math.random()*0.5+0.5,Math.random()*0.5+0.5,Math.random()*0.5+0.5)
-
-gl.drawArrays(gl.TRIANGLES,0,3)
--- hen/spiritual_body/src/main.js
+++
@@ -1,9 +0,0 @@
-resize(innerWidth, innerHeight)
-uf1('time', Math.random()*827464);
-gl.drawArrays(gl.TRIANGLES,0,3)
-
-
-
-
-
-
