392b4ccfac2b17280d771327edb908663c54e971
k.balashev <k.balashev@geoscan.aero>
2021-10-11 10:29:31 +0300

    cleanup

--- coco/ScreenSpacePathTest.js
+++
@@ -1,8 +0,0 @@
-var PrimitivesMath = require('../scripts/objects/primitives/PrimitivesMath');
-
-describe("#lineDirection", function () {
-    it("lineDirection 1,1 must be 45", function () {
-        var dir = PrimitivesMath.lineDirection(1, 1);
-        expect(dir).toBe(45);
-    });
-});
