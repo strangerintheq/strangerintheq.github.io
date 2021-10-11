f3e05909ffc3fc2effdea195443166299bf80288
k.balashev <k.balashev@geoscan.aero>
2021-10-11 15:13:58 +0300

    refactor

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-
 let prefix = '';
 if (document.location.href.indexOf("localhost") > -1)
     prefix = '/strangerintheq.github.io'
-
 
 async function load(){
     const settings = await (await fetch(prefix + '/settings.json')).json();
@@ -10,20 +8,28 @@ async function load(){
     document.querySelector('.avatar')
         .setAttribute('src', prefix + '/' + settings.avatar);
 
-    document.querySelector('.projects').innerHTML = settings.projects.map(project => {
-        return `<a data-project="${project[0]}"
-                    onclick="setProject(this, '${prefix + project[1]}')">${project[0]}</a>`
-    }).join('');
+    document.querySelector('.projects').innerHTML =
+        settings.projects.map(asProject).join('');
 
-    document.querySelector('.links').innerHTML = settings.links.map(project => {
-        return `<a href="${project[2]}" target="_blank">
-            <img src="${prefix + '/resources/'+project[0]}" title="${project[1]}">
-        </a>`;
-    }).join('');
+    document.querySelector('.links').innerHTML =
+        settings.links.map(asLink).join('');
+
+    init();
 
 }
 
 load();
+
+function asProject(project) {
+    return `<a data-project="${project[0]}"
+        onclick="setProject(this, '${prefix + project[1]}')">${project[0]}</a>`
+}
+
+function asLink(link){
+    return `<a href="${link[2]}" target="_blank">
+        <img src="${prefix + '/resources/'+link[0]}" title="${link[1]}">
+    </a>`;
+}
 
 function setProject(link, url) {
     let selected = document.querySelector('.selected');
@@ -33,10 +39,12 @@ function setProject(link, url) {
     history.pushState({}, url, prefix+'/?project='+link.dataset.project)
 }
 
-if (document.location.search) {
-    const proj = new URL(location.href).searchParams.get('project');
-    document.querySelector(`a[data-project="${proj}"]`).click()
-} else {
-    [...document.querySelectorAll('.projects a')]
-        .sort(() => Math.random() - 0.5)[0].click()
+function init() {
+    if (document.location.search) {
+        const proj = new URL(location.href).searchParams.get('project');
+        document.querySelector(`a[data-project="${proj}"]`).click()
+    } else {
+        [...document.querySelectorAll('.projects a')]
+            .sort(() => Math.random() - 0.5)[0].click()
+    }
 }
