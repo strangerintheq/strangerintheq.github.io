4012a6756636fed9b2cbfd517ab61048168ed434
k.balashev <k.balashev@geoscan.aero>
2021-10-13 11:41:21 +0300

    refactor

--- script.js
+++ script.js
@@ -5,18 +5,17 @@ if (document.location.href.indexOf("localhost") > -1)
 
 async function load(){
     settings = await (await fetch(prefix + '/settings.json?'+Date.now())).json();
-
     document.querySelector('.avatar')
         .setAttribute('src', prefix + '/' + settings.avatar);
-
-    document.querySelector('.projects').innerHTML =
-        settings.projects.map(asProject).join('');
-
-    document.querySelector('.links').innerHTML =
-        settings.links.map(asLink).join('');
-
+    fill('.projects', settings.projects.released, asProject)
+    fill('.links-nft', settings.links.nft, asLink)
+    fill('.links-social', settings.links.social, asLink)
     init();
+}
 
+function fill(selector, array, mapFn) {
+    document.querySelector(selector).innerHTML
+        = array.map(mapFn).join('');
 }
 
 load();
@@ -27,9 +26,7 @@ function asProject(project) {
 }
 
 function asLink(link){
-    return `<a href="${link[2]}" target="_blank">
-        <img src="${prefix + '/resources/'+link[0]}" title="${link[1]}">
-    </a>`;
+    return `<a href="${link[2]}" target="_blank"><img src="${prefix + '/resources/'+link[0]}" title="${link[1]}"></a>`;
 }
 
 function setProject(link, url) {
@@ -43,7 +40,7 @@ function setProject(link, url) {
 function init() {
     if (document.location.search) {
         const proj = new URL(location.href).searchParams.get('p');
-        let url = [...settings.projects, ...settings.upcoming]
+        let url = [...settings.projects.released, ...settings.projects.upcoming]
             .find(p => p[0] === proj)[2];
         document.querySelector('iframe').src = url;
         // document.querySelector(`a[data-project="${proj}"]`).click()
