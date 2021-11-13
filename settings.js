62073c7aac98e82def55c670b1f41f814222bca0
Konstantin Balashev <tripmannn@gmail.com>
2021-11-13 13:50:43 +0300

    needlework

--- settings.js
+++ settings.js
@@ -1,5 +1,6 @@
 window.settings = {
   "projects":  [
+      ["NDLW", "Needlework"],
       ["WARP", "Warp Engine"],
       ["HDSQ", "Hidden Square"],
       ["NRIF", "Neural Interface"],
@@ -13,7 +14,7 @@ window.settings = {
   ],
   "links": {
     "nft": [
-      ["hicetnunc.svg", "Hic Et Nunc", "https://www.hicetnunc.xyz/Stranger_in_the_Q"],
+      ["fxhash.svg", "FxHash", "https://www.fxhash.xyz/u/StrangerintheQ"],
       ["objkt.png", "Objkt.com", "https://objkt.com/profile/tz2MHcnM7fEVtExELMPBZb5njDweqMGvbn8A"],
       ["opensea.svg", "Opensea", "https://opensea.io/Stranger_in_the_q"],
       ["foundation.svg", "Foundation", "https://foundation.app/@Stranger_in_the_Q"],
