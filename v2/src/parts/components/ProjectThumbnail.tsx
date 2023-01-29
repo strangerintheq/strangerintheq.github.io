1a34d69c63527e7d040a0196e9e59ace4274819d
Konstantin Balashev <tripmannn@gmail.com>
2023-01-29 22:01:27 +0300

    site v2

--- v2/src/parts/components/ProjectThumbnail.tsx
+++ v2/src/parts/components/ProjectThumbnail.tsx
@@ -2,18 +2,18 @@ import React from "react";
 import {Link} from "react-router-dom";
 import {sitePath} from "../Crutch";
 
-export function ProjectThumbnail({name, url, img, id, ratio=1}) {
+export function ProjectThumbnail({name, about=null, img, generator, ratio=1}) {
     const backgroundImage = `url(${img.startsWith("http") ? img: sitePath + img})`
     const height = `calc(var(--base-size) * ${0.25/(ratio||1)})`;
     return <div>
-        <Link to={'/generator/' + id}>
+        <Link to={'/generator/' + generator}>
             <div
                 className={"project-thumbnail"}
                 style={{backgroundImage, height}}
             >
                 <div className={"project-info"}>
                     <div className={"fs02"}>{name}</div>
-                    <Link to={url}><div className={"fs015"}>About</div></Link>
+                    {about&&<Link to={about}><div className={"fs015"}>About</div></Link>}
                 </div>
             </div>
 
