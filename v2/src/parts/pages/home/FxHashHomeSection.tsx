924408f67f65480911a3feeb9811b3b26b44adee
k.balashev <k.balashev@geoscan.aero>
2023-02-20 15:31:58 +0300

    site.v2

--- v2/src/parts/pages/home/FxHashHomeSection.tsx
+++ v2/src/parts/pages/home/FxHashHomeSection.tsx
@@ -4,24 +4,25 @@ import {ProjectItem} from "../../components/ProjectItem";
 
 export function FxHashHomeSection(props) {
     return <div style={{marginTop:80,  }}>
-        <h2><span>fx(hash)</span></h2>
+        <h2><span>Footer</span></h2>
         <div style={{
             marginTop: 40,
+            marginBottom: 40,
             display: "grid",
             gap: "2%",
             gridTemplateColumns: "23.5% 23.5% 23.5% 23.5%"
         }}>
 
-                <ProjectItem
-                    w={0.5}
-                    h={0.5}
+                {/*<ProjectItem*/}
+                {/*    w={0.5}*/}
+                {/*    h={0.5}*/}
 
-                    hash={"0xc769de01c92fdf68c8415137f67e0b34869f42e7ae170f3eb81a192a67f8c930"}
-                    type={"artblocks"}
-                    name={"SL/CE"}
-                    img={"https://res.cloudinary.com/art-blocks/image/fetch/f_auto,c_limit,w_3840,q_auto/https://artblocks-mainnet.s3.amazonaws.com/419000000.png"}
-                    generator={'slice'}
-                    />
+                {/*    hash={"0xc769de01c92fdf68c8415137f67e0b34869f42e7ae170f3eb81a192a67f8c930"}*/}
+                {/*    type={"artblocks"}*/}
+                {/*    name={"SL/CE"}*/}
+                {/*    img={"https://res.cloudinary.com/art-blocks/image/fetch/f_auto,c_limit,w_3840,q_auto/https://artblocks-mainnet.s3.amazonaws.com/419000000.png"}*/}
+                {/*    generator={'slice'}*/}
+                {/*    />*/}
 
         </div>
     </div>
