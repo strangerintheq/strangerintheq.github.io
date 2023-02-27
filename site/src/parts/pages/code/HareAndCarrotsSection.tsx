f897053ef6a9c43161649598f849d8c58f9cccd7
k.balashev <k.balashev@geoscan.aero>
2023-02-27 11:15:02 +0300

    hare images, dots images

--- site/src/parts/pages/code/HareAndCarrotsSection.tsx
+++ site/src/parts/pages/code/HareAndCarrotsSection.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import {PageSubHeader} from "../../components/PageSubHeader";
 import {PageText} from "../../components/PageText";
 import {ExternalLink} from "../../components/ExternalLink";
+import {Grid} from "../../components/Grid";
+import {DivWithBackgroundImage} from "../../components/DivWithBackgroundImage";
 
 export function HareAndCarrotsSection() {
     return <>
@@ -19,6 +21,11 @@ export function HareAndCarrotsSection() {
             <ExternalLink href={"https://github.com/strangerintheq/hare-and-carrots"}>single player</ExternalLink>
             &nbsp;
             <ExternalLink href={"https://github.com/strangerintheq/hare-networked"}>multiplayer</ExternalLink>
+            <Grid>
+                <DivWithBackgroundImage img={"site/resources/code/hare-0.jpg"}/>
+                <DivWithBackgroundImage img={"site/resources/code/hare-1.jpg"}/>
+                <DivWithBackgroundImage img={"site/resources/code/hare-2.jpg"}/>
+            </Grid>
         </PageText>
     </>
 }
