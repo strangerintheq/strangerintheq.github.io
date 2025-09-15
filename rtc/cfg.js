15a3a8f2b8da7079cd7782b84b5f096f392b6563
Konstantin Balashev <tripmannn@gmail.com>
2025-09-15 14:22:16 +0300

    rtc

---
+++ rtc/cfg.js
@@ -0,0 +1,25 @@
+window.cfg = {
+    iceServers: [
+        {
+            urls: 'stun:stun.l.google.com:19302'
+        },
+        {
+            urls: 'stun:81.200.147.103:3478'
+        },
+        {
+            urls: 'turn:global.relay.metered.ca:80',
+            username: 'f6507426c0f4f89d0bda02e2',
+            credential: '7YF0907XexAfvkbL',
+        },
+        {
+            urls: 'turn:global.relay.metered.ca:443',
+            username: 'f6507426c0f4f89d0bda02e2',
+            credential: '7YF0907XexAfvkbL',
+        },
+        {
+            urls: 'turn:81.200.147.103:3478',
+            username: 'chessclubturn',
+            credential: 'vhSsAm602ITECZv1',
+        },
+    ]
+}
